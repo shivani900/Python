f1103522972b78a7917f72d5789dbab5a760562a
shivaniferia <>
2022-03-24 11:09:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,296 @@
+# a='arujmahajan'
+# print(a)
+# b='shivaniferia'
+# print(b)
+# print('"abgfghggc"')
+# a='''ghggghghghggh
+# hhhh
+# hhhhhhh'''
+# '''
+# hjdhjdgcfgyfffbcccbbccbcb
+# hghghgggggggggggfgcfffrdtt
+# hhhhh
+# '''
+
+# # print(a)
+# q=True
+# print(3>5)
+# print(10+10==54)
+# print(44-4==48)
+# print(44+4==48)
+# a=1234
+# b=55.67
+# c=None
+# d='shivani'
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# 'assigment' 
+
+# a = 34
+# a += 1
+# print(a)
+# o=100
+# o+=34
+# print(o)
+# q=56
+# q+=2
+# print(q)
+#print((3>1) and (4<1))
+#print((4<6)and   (7>12))
+# # print((66>5)and(44>2))
+# a=567
+# print(a+5)
+# print(a)
+# a+=5
+# print(a)
+# a="shivani"
+
+# print(type(a))
+# a="4"
+# a = int(a)
+# print(a+4
+# a=input("enter your age")
+# print(a)
+# b=input("your profession")
+# print(b)
+# c=input("enter your husband name")
+# print(c)
+# 
+# 
+# a=17
+# b=8
+# print(a%b)
+# a=input("what is the time")
+# print(type(a))
+# a=34
+
+# b=80
+# print(a>b)
+# a=input("enter first number ")
+# b=input("enter second number ")
+# a=int(a)
+# b=int(b)
+# print((a+b)/2)
+# a=input('enter a number ')
+# a=int(a)
+# print(a*a)
+##a=5
+#print(a==5)""
+#print(10==10)
+# a=input("what's your name?")
+# print(a)
+# a=input("first number ")
+# b=input(" second number ")
+# a=int(a)
+# b=(int(b))
+# print(a//b)
+#english= int(input("enter your number "))
+# hindi=int(input("enter your number "))
+# science=int(input("enter your number"))
+# social_studies=int(input("enter your number "))
+# physical_education =int(input("enter your number "))
+# total_marks= 500
+# my_marks= english + hindi + science + social_studies + physical_education 
+# percentage = (my_marks/total_marks)*100
+# print(percentage)
+# q=int(input("enter first number "))
+# e=int(input("enter second number "))
+# print(q>e)
+# a=int(input("enter first number "))
+# b=int(input("enter second number "))
+# c=int(input("enter third number "))
+# print((a+b+c)-500)
+# a=500
+# a+=100
+# print(a)
+# a=9
+# b=4
+# c=a%b
+# d=a//b
+# print(c+d)
+# name="shivani"
+# print(name[0])
+# name= "shivani"
+# print(name[0:2])
+# name ="shivani"
+# name[6]="a"
+# print(name) 
+# name="hgghguhhjjvjhhgvbbhhhufhhhuuutuu"
+# print(name[-1])
+# name='hgfygyrfgriufoefifehrfvehcbjhfififfiuhvbhuh'
+# # print(name[-6])
+# print(name[-1:-10]) 
+# name="hdhhdhengftyfeuhggguehhguhgh"
+# print(name[-10:-4]) 
+# name="shivani_feria"
+# print(name[-7:-4]) 
+# name="shivaniferia"
+# print(name[-10])
+# name=("shivaniferia")
+# print(name[-3:-10])
+# name=input("enter your name")
+# print("good afternoon" + name)
+# name=input("what's
+#            your name")
+# q="good afternoon "
+# print(q+name)
+# name=input("enter your name ")
+# date=input("enter date")
+# letter="dear " + name + " congratulation!!!!\n you are selected " +date
+# print(letter)
+# a="my name is  shivani feria wife of Mr Aruj mahajan"
+# a=a.replace("  ", " ")
+# print(a)
+# fruits= []
+# fruit1=input("enter 1st fruit ")
+# fruit2=input("enter 2nd fruit ")
+# fruit3=input("enter 3rd fruit ")
+# fruit4=input("enter 4th fruit")
+# fruits.append(fruit1)
+# fruits.append(fruit2)
+# fruits.append(fruit3)
+# fruits.append(fruit4)
+# print(fruits)
+# student1= input("enter your marks ")
+# student2= input("enter your marks ")
+# student3= input("entter your marks")
+# student4= input("enter your marks ")
+# student5= input("enter your marks ")
+# student6= input("enter your marks ")
+# student7=
+#input("enter your marks ")
+# marks=[student1 , student2 , student3 , student4 , student5 , student6 , student7 ]
+# marks.sort()
+# print(marks) 
+# t=(1,2,3,4,5,6,7,8,9,10)
+# t(0=99)
+# print(t)
+# L=[12,34,45,55,66]
+# s=sum(L)
+# print(s) 
+# a=(7,0,8,0,0,9)
+# s=a.count(0)
+# print(s)
+# a={
+#   "akash": 99,
+#   "eve": 67,
+#   'divi': 88,
+#   "shiv" : 77
+# }
+# b=input("enter your name")
+# print(a.get(b)) 
+# age=int(input("enter your age"))
+# if(age>=18):
+#   print("yes")
+# else:
+#   print("no")
+# number=int(input("enter your number "))
+# number1=int(input("enter your number "))
+# number2=int(input("enter your number"))
+# number3=int(input("enter your number"))
+# if(number>number1 and number>number2 and number>number3 ):
+#   print("first number is greatest")
+# elif(number1>number and number1>number2 and number1>number3 ):
+#    print("second number is greatest")
+# elif(number2>number and number2>number1 and number2>number3 ):
+#    print("third number is greatest")
+# elif(number3>number and number3>number2 and number3>number1 ):
+#    print("fourth number is greatest")
+# subject1=int(input("enter your marks "))
+# subject2=int(input("enter your marks "))
+# subject3=int(input("enteryour number "))
+# total_marks=subject1 + subject2 +subject3 
+# percentage=(total_marks/3)*100
+# if(percentage >= 40 and subject1>=33 and subject2>=33 and subject3>=33):
+#   print("pass")
+# else:
+#   print("fail")  
+# text=input("enter your sentence")
+# if("make a lot of money" in text):
+#   print("this is a spam")
+# elif("buy now" in text):
+#   print("this is a spam")
+# elif("subcribe now" in text):
+#   print("this is a spam")
+# elif("click this" in text):
+#   print("this is a spam")
+# else:
+#   print("this is not a spam")
+# name=input("enter your username ")
+# username_length=len(name)
+# if(username_length < 10):
+#   print("lesser")
+# else:
+#   print("greater")
+# names=["rohit", "simran", "ravi", "shalini","gupta", "mahajan"]
+# name=input("enter your name")
+# if name in names:
+#   print("your name is present in the list")
+# else:
+#   print("your name is not present in the list")ks
+# marks=int(input("enter your marks"))
+# if(marks>90 and marks<=100):
+#   print("ex")
+# elif(marks>80 and marks<=90):
+#   print("a")
+# elif(marks>70 and marks<=80):
+#   print("b")
+# elif(marks>60 and marks<=70):
+#   print("c")
+# elif(marks>50 and marks<=60):
+#   print("d")
+# else:
+#   print("f")
+# / 
+# a=1
+# while(a<50):
+#   print(a)
+#   a=a+1
+# fruits=["mango", "apple","banana","grapes","pineapple"]
+# i=0
+# while i < len(fruits):
+#   print(fruits[i])
+#   i=i+
+# number=int(input("enter your table"))
+# for i in range(1,11):
+#   print(i*number)
+# fruits=["mango", "apple","banana","grapes","pineapple"]
+# for i in range(len(fruits)):
+#   print(fruits[i])
+# li=("harry" , "sohan", "sachin", "rahul")
+# for i in range(len(li)):
+#   if (li[i][0]== "s"):
+#     print("hello " +li[i])
+# number=int(input("enter your number"))
+# for i in range(2,number):
+#   if(number % i == 0):
+#     print("not prime")
+#     break
+# else:
+#   print("prime")
+# number=int(input("enter your number"))
+# a=1
+# s=0
+# while(a<=number):
+#   s=s+a
+#   a=a+1
+# print(s)
+# number=int(input('enter your number'))
+# s=0
+# for i in range(1,number+1):
+#   s=s+i
+# print(s)
+# number=int(input("enter your number"))
+# a=1
+# for i in range(1,number+1):
+#   a=a*i
+# print(a)
+# n=6
+# for i in range(1,n+1):
+#   print("*" * i)
+number=int(input("enter your number"))
+a=10
+for i in range(10,1,-1):
+  print(number*i)
