bafd45d692ad0cdcbf16ee2abd03fb14840b7d93
shivani feria <102295691+shivani900@users.noreply.github.com>
2022-03-25 07:43:38 +0000

    problem 2

---
+++ Problemset5/problem2.py
@@ -0,0 +1,11 @@
+# Write a program to input eight numbers from the user and display all the unique numbers (once
+s1=int(input("enter your number"))
+s2=int(input("enter your number"))
+s3=int(input("enter your number"))
+s4=int(input("enter your number"))
+s5=int(input("enter your number"))
+s6=int(input("enter your number"))
+s7=int(input("enter your number"))
+s8=int(input("enter your number"))
+a={s1,s2,s3,s4,s5,s6,s7,s8}
+print(a)
