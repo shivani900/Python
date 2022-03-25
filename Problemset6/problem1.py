1b009cf69bfa625edc055db1f28ad6565a8db4c1
shivani feria <102295691+shivani900@users.noreply.github.com>
2022-03-25 11:17:02 +0000

    problem 1

--- Problemset6/problem1.py
+++ Problemset6/problem1.py
@@ -3,11 +3,11 @@ n1=int(input("enter your number "))
 n2=int(input("enter your number "))
 n3=int(input("enter your number "))
 n4=int(input("enter your number "))
-if(n1>n2, n1>n3, n1>n4):
+if(n1>n2 and n1>n3 and n1>n4):
   print(f"greatest number is {n1} ")
-elif(n2>n1, n2>n3, n2>n4):
+elif(n2>n1 and n2>n3 and n2>n4):
   print(f"greatest number is {n2}")
-elif(n3>n1, n3>n2, n3>n4):
+elif(n3>n1 and n3>n2 and n3>n4):
   print(f"greatest number is {n3}")
 else:
   print(f"greatestnumber is {n4}")
