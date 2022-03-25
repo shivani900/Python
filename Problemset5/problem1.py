cb92a64d580f7668b0f4bc43db9d091ad731932f
shivani feria <102295691+shivani900@users.noreply.github.com>
2022-03-25 07:28:35 +0000

    problem 1

---
+++ Problemset5/problem1.py
@@ -0,0 +1,8 @@
+# Write a program to create a dictionary of Hindi words with values as their English translation. Provide the user with an option to look it up!
+dic = {"chabi" : "keys",
+"chai" : "tea",
+"copy": "notebook",
+"ladka": "boy",
+"ladki": "girl"}
+key=input("enter your hindi word ")
+print(dic[key])
