826e20337e3442e00eae54487c64f143704b64ee
shivani feria <102295691+shivani900@users.noreply.github.com>
2022-03-25 11:10:15 +0000

    problem 1

---
+++ Problemset6/problem1.py
@@ -0,0 +1,13 @@
+#Write a program to find the greatest of four numbers entered by the user.
+n1=int(input("enter your number "))
+n2=int(input("enter your number "))
+n3=int(input("enter your number "))
+n4=int(input("enter your number "))
+if(n1>n2, n1>n3, n1>n4):
+  print(f"greatest number is {n1} ")
+elif(n2>n1, n2>n3, n2>n4):
+  print(f"greatest number is {n2}")
+elif(n3>n1, n3>n2, n3>n4):
+  print(f"greatest number is {n3}")
+else:
+  print(f"greatestnumber is {n4}")
