2f5792458b575d248883b870b8959956a974ff47
shivani feria <102295691+shivani900@users.noreply.github.com>
2022-03-25 06:48:28 +0000

    problem 2

---
+++ problemset4/problem2.py
@@ -0,0 +1,9 @@
+s1=int(input("enter your marks "))
+s2=int(input("enter your marks "))
+s3=int(input("enter your marks "))
+s4=int(input("enter your marks "))
+s5=int(input("enter your marks "))
+s6=int(input("enter your marks "))
+a=[s1,s2,s3,s4,s5,s6]
+a.sort(reverse=True)
+print(a)
