b3816c49bb31761fe1d8c3993aeb3323a7ebabff
shivani feria <102295691+shivani900@users.noreply.github.com>
2022-03-25 06:37:22 +0000

    problem 1

---
+++ problemset4/problem1.py
@@ -0,0 +1,9 @@
+f1=input("enter your fruit name ")
+f2=input("enter your fruit name ")
+f3=input("enter your fruit name ")
+f4=input("enter your fruit name ")
+f5=input("enter your fruit name ")
+f6=input("enter your fruit name ")
+f7=input("enter your fruit name ")
+l=[f1,f2,f3,f4,f5,f6,f7]
+print(l)
