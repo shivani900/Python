3050f5ebbf677424d3f3d30ddb36749df4fe8065
shivani feria <102295691+shivani900@users.noreply.github.com>
2022-03-26 07:45:06 +0000

    problem 1

---
+++ Problemset7/problem1.py
@@ -0,0 +1,6 @@
+#Write a program to print the multiplication table of a given number using for loop.
+t1=int(input("enter your number "))
+for i in range(1,11):
+  print(i)
+  print(f"{t1} X {i} = {t1*i}")
+
