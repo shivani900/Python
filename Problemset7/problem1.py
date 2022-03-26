5e26f1afe7c8c6235a98a95420d5a1e908805665
shivani feria <102295691+shivani900@users.noreply.github.com>
2022-03-26 11:41:35 +0000

    problem 2

--- Problemset7/problem1.py
+++ Problemset7/problem1.py
@@ -3,4 +3,4 @@ t1=int(input("enter your number "))
 for i in range(1,11):
   print(i)
   print(f"{t1} X {i} = {t1*i}")
-
+i=int(input("enter your number"))
---
+++ Problemset7/problem2.py
@@ -0,0 +1,8 @@
+# Write a program to greet all the person names stored in a list l1 and which starts with S.
+l1 = ["Harry", "Sohan", "Sachin", "Rahul"]
+a= "Good afternoon "
+for i in l1:
+  if(i[0]=="S"):
+    print(a+i)
+    
+    
