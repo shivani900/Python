1b995267c857d85ffa4e1b7ef4f69fe66c1f397d
shivani feria <102295691+shivani900@users.noreply.github.com>
2022-03-26 17:57:54 +0000

    Problem 6

--- Problemset7/problem5.py
+++ Problemset7/problem5.py
@@ -1,8 +1,8 @@
 # Write a program to find the sum of first n natural numbers using a while loop.
-n=int(input("enter your number"))
+n=int(input("enter your number "))
 a=1
 s=0
 while(a<n+1):
   s=s+a
   a=a+1
-print(s)
+print(s) 
---
+++ Problemset7/problem6.py
@@ -0,0 +1,11 @@
+# Write a program to calculate the factorial of a given number using for loop.
+n=int(input("enter your number "))
+m=1
+for i in range(1,n+1):
+  m=m*i
+print(m)
+
+
+
+    
+    
